d31e220b7b0f92c807614ab52288072609389c00
maxkonrad <furkanselek@outlook.com>
2023-06-11 00:30:14 +0300

    init RC component

---
+++ vue-frontend/src/components/RemoteControl.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="player-remote">
+    <button @click="toggleOperate">Operate</button>
+    <button @click="volumeUp">Volume Up</button>
+    <button @click="volumeDown">Volume Down</button>
+    <button @click="play">Play</button>
+    <button @click="stop">Stop</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleOperate() {
+        
+    },
+    volumeUp() {
+    },
+    volumeDown() {
+    },
+    play() {
+    },
+    stop() {
+    },
+  },
+  data() {
+    return {
+        timer: null,
+    }
+  }
+};
+</script>
+
+<style scoped>
+    .player-remote{
+        text-align: center;
+    }
+
+    button {
+        font-size: larger;
+    }
+</style>
