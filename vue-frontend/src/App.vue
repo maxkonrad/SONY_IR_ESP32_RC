1c0c61df2adb03052700d3a676ae12067eb922fb
maxkonrad <furkanselek@outlook.com>
2023-06-11 00:30:02 +0300

    init App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+import RemoteControl from './components/RemoteControl.vue';
+
 </script>
 
 <template>
-  
+  <div id="app">
+    <h1 class="rc" >SONY CD Oynatıcı için Uzaktan Kumanda</h1>
+    <RemoteControl />
+  </div>
 </template>
 
 <style scoped>
-
+  .rc {
+    text-align: center;
+  }
 </style>
